80273f91baf7c6d50ff6301a9628c5d30b94fb18
Hondallica <hondallica@gmail.com>
2014-08-14 22:41:26 +0900

    Added api_key and http attributes

--- lib/WebService/MusixMatch.pm
+++ lib/WebService/MusixMatch.pm
@@ -1,10 +1,38 @@
 package WebService::MusixMatch;
-use 5.008001;
-use strict;
-use warnings;
-
+use JSON::XS;
+use Cache::LRU;
+use Net::DNS::Lite;
+use Furl;
+use URI;
+use URI::QueryParam;
+use Carp;
+use Moo;
+use namespace::clean;
 our $VERSION = "0.01";
 
+
+$Net::DNS::Lite::CACHE = Cache::LRU->new( size => 512 );
+
+has 'api_key' => (
+    is => 'rw',
+    isa => sub { $_[0] },
+    required => 1,
+    default => $ENV{MUSIXMATCH_API_KEY},
+);
+
+has 'http' => (
+    is => 'rw',
+    required => 1,
+    default  => sub {
+        my $http = Furl::HTTP->new(
+            inet_aton => \&Net::DNS::Lite::inet_aton,
+            agent => 'Net::LastFM::Lite' . $VERSION,
+            headers => [ 'Accept-Encoding' => 'gzip',],
+        );
+        $http->env_proxy;
+        return $http;
+    },
+);
 
 
 1;
